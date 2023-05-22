3e0f60d43576f0d6957455e3407ac6a44d2c3906
LuongLuyen <luongvanluyen2003@gmail.com>
2023-05-22 16:35:52 +0700

    deploy

---
+++ build/models/HomeB.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Schema = new mongoose.Schema({
+  status_1: {
+    type: String,
+    required: true
+  },
+  status_2: {
+    type: String,
+    required: true
+  },
+  status_3: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+module.exports = mongoose.model("HomeB", Schema);
---
+++ build/models/HomeQ.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Schema = new mongoose.Schema({
+  status_1: {
+    type: String,
+    required: true
+  },
+  status_2: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+module.exports = mongoose.model("HomeQ", Schema);
